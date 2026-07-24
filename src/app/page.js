bbacb949ec71e34844282b3ba4a08afdc0bcc799
nahin113 <nahinahmed113@gmail.com>
2026-07-25 02:39:46 +0600

    feat: implemented about section with minor animations of texts and images coming from opacity 0 one by one, this ensures better UI experience

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import About from "@/components/About";
 import Cocktails from "@/components/Cocktails";
 import Hero from "@/components/Hero";
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <>
       <Hero />
       <Cocktails/>
+      <About/>
     </>
   );
 }
