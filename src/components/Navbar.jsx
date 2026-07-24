79417605335f681283f161ea65b65a0ed17744e0
nahin113 <nahinahmed113@gmail.com>
2026-07-24 21:12:52 +0600

    feat: implemented the navbar section of the application with transparent background and blury on scroll with gsap for modern cooler experience of the users

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+'use client'
+import Link from "next/link";
+import React from "react";
+import { navLinks } from "../../constants";
+import Image from "next/image";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+
+const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
+
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+  });
+  return (
+    <nav>
+      <div>
+        <Link href="#home" className="flex items-center gap-2">
+          <Image
+            src="/images/logo.png"
+            alt="logo"
+            height={35}
+            width={35}
+          ></Image>
+          <p>Velvel Pour</p>
+        </Link>
+
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <Link href={`#${link.id}`}>{link.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
