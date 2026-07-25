189836ed6bf481235f387eb741452ba499607ffc
nahin113 <nahinahmed113@gmail.com>
2026-07-25 16:52:33 +0600

    feat: implemented menu section with smooth animations for the content description and images

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import About from "@/components/About";
 import Art from "@/components/Art";
 import Cocktails from "@/components/Cocktails";
 import Hero from "@/components/Hero";
+import Menu from "@/components/Menu";
 export default function Home() {
   return (
     <>
@@ -9,6 +10,7 @@ export default function Home() {
       <Cocktails/>
       <About/>
       <Art/>
+      <Menu/>
     </>
   );
 }
